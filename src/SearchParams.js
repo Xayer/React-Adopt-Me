999c14d65a61018802dd2618cc4504366979e29f
Frederik Rabøl Madsen <frm@cardlay.com>
2021-02-17 10:27:14 +0100

    added searchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const location = "Seatle, WA";
+  // THIS IS A HOOK! All Hooks start with "use"
+  const [location, setLocation] = useState("Seatle, WA"); //Default state for the value.
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  // DO NOT USE IF/CONDITIONAL STATEMENTS AROUND HOOKS! React is keeping track of the order that you call the hooks in.
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option value={breedString} key={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
